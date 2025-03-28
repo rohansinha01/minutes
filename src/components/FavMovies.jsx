eb0c36112ceaa6fe6e257c28c223d96d782365e3
rohansinha01 <rohansinha01@gmail.com>
2025-03-28 13:46:13 -0400

    fave

---
+++ src/components/FavMovies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavMovies = () => {
+  return (
+    <div>FavMovies</div>
+  )
+}
+
+export default FavMovies
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import FavMovies from "./FavMovies";
 
 const MovieList = (props) => {
   return (
@@ -9,6 +10,9 @@ const MovieList = (props) => {
         <div className="image-container d-flex justify-content-start m-3">
           {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
           <img src={movie.Poster} alt="movie"></img>
+          <div className="overlay d-flex align-items-center justify-content-center">
+            <FavMovies />
+          </div>
         </div>
       ))}
     </>
